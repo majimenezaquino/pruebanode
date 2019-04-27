eeaa6e28b61da421924e88b9b948d019be3d2a85
Miguel <migueljimenezaquino@gmail.com>
2019-04-27 18:56:14 -0400

    change

--- serverapp.js
+++ serverapp.js
@@ -1,76 +1,77 @@
-' uso estricto ' ;
 
-const  Compute  =  require ( ' @ google-cloud / compute ' );
-const  http  =  require ( ' http ' );
+'use strict';
 
-const  compute  =  new  Compute ();
+const Compute = require('@google-cloud/compute');
+const http = require('http');
 
-const  zone  =  computar . zona ( ' us-central1-a ' );
+const compute = new Compute();
 
-// Crear una nueva máquina virtual, utilizando la imagen ubuntu predeterminada. El script de inicio
-// instala Node e inicia un servidor Node.
-const  config  = {
-  os :  ' ubuntu ' ,
-  http :  true ,
-  metadatos : {
-    artículos : [
+const zone = compute.zone('us-central1-a');
+
+// Create a new VM, using default ubuntu image. The startup script
+// installs Node and starts a Node server.
+const config = {
+  os: 'ubuntu',
+  http: true,
+  metadata: {
+    items: [
       {
-        clave :  ' startup-script ' ,
-        valor :  ` #! / bin / bash
-        # Obtener el administrador de versiones de Node e instalar Node 8.
-        curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | golpetazo
-        export NVM_DIR = "$ HOME / .nvm"
-        [-s "$ NVM_DIR / nvm.sh"] && \. "$ NVM_DIR / nvm.sh" # Esto carga nvm
+        key: 'startup-script',
+        value: `#! /bin/bash
+        # Get Node version manager and install Node 8.
+        curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | bash
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
         nvm install 8
-        # Instalar git
-        apt-get --assume-yes instala git
-        # Clonar la aplicación de muestra e iniciarla.
+        # Install git
+        apt-get --assume-yes install git
+        # Clone sample application and start it.
         git clone https://github.com/fhinkel/nodejs-hello-world.git
         cd nodejs-hello-world
-        npm start & `
-      }
-    ]
-  }
+        npm start &`
+      },
+    ],
+  },
 };
 
-const  vm  =  zona . vm ( ' vm-with-node-server ' );
+const vm = zone.vm('vm-with-node-server');
 
-( async () => {
-  prueba {
-    const  data  =  esperar  vm . crear (config);
-     operación  const = datos [ 1 ];
-    la espera de  la operación . promesa ();
+(async () => {
+  try {
+    const data = await vm.create(config);
+    const operation = data[1];
+    await operation.promise();
 
-    // IP externa de la máquina virtual.
-     metadatos  const =  esperar  vm . getMetadata ();
-    const  ip  = metadata [ 0 ]. networkInterfaces [ 0 ]. accessConfigs [ 0 ]. natIP ;
-    consola . log ( ` Arrancando una nueva VM con IP http: // $ { ip } ... ` );
+    // External IP of the VM.
+    const metadata = await vm.getMetadata();
+    const ip = metadata[0].networkInterfaces[0].accessConfigs[0].natIP;
+    console.log(`Booting new VM with IP http://${ip}...`);
 
-    // Haga ping a la VM para determinar cuándo está listo el servidor HTTP.
-    dejar en espera =  verdadero ;
-     temporizador de  const =  setInterval (
-      ip  => {
+    // Ping the VM to determine when the HTTP server is ready.
+    let waiting = true;
+    const timer = setInterval(
+      ip => {
         http
-          . get ( ' http: // '  + ip, res  => {
-            const  statusCode  =  res . statusCode ;
-            if (statusCode ===  200  && esperando) {
-              esperando =  falso ;
-              clearTimeout (temporizador);
-              // El servidor HTTP está listo.
-              consola . log ( ' ¡Listo! ' );
-              consola . log (ip);
+          .get('http://' + ip, res => {
+            const statusCode = res.statusCode;
+            if (statusCode === 200 && waiting) {
+              waiting = false;
+              clearTimeout(timer);
+              // HTTP server is ready.
+              console.log('Ready!');
+              console.log(ip);
             }
           })
-          . en ( ' error ' , () => {
-            // El servidor HTTP no está listo todavía.
-            proceso . la salida estándar . escribir ( ' . ' );
+          .on('error', () => {
+            // HTTP server is not ready yet.
+            process.stdout.write('.');
           });
-      }
-      2000 ,
+      },
+      2000,
       ip
     );
   }
-  atrapar (error) {
-    consola . error (error);
+  catch (error) {
+    console.error(error);
   }
-}) ();
+})();
